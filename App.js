39e8a232bd56a31a1bd132d11fa4eaec5baec75f
Tarik Kdiry <tarik.kdiry@gmail.com>
2020-06-18 19:36:46 -0400

    init develop branch

--- App.js
+++ App.js
@@ -1,15 +1,61 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// EXPO
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
 // NAVIGATION
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// SCREENS
+import WelcomeScreen from './src/screens/Welcome';
+import SelectScreen from './src/screens/Select';
+
+const Stack = createStackNavigator();
+
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  // FETCH FONTS
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      'bold': require('./assets/fonts/Montserrat/Montserrat-Bold.ttf'),
+      'regular': require('./assets/fonts/Montserrat/Montserrat-Black.ttf'),
+    });
+  };
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen 
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen 
+          name="Select"
+          component={SelectScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/screens/Select.js
+++ src/screens/Select.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-const WelcomeScreen = ({ route, navigation }) => {
+const SelectScreen = ({ route, navigation }) => {
     return (
         <View style={styles.container}>
             <View style={styles.top}>
-                <Text style={styles.text}>Welcome</Text> 
+                <Text style={styles.text}>Select</Text> 
             </View>
             <View style={styles.bottom}>
                 <Button 
-                    title="Let's get started"
+                    title="Back"
                     onPress={() => {
-                        navigation.push('Select', {
-
-                        })
+                        navigation.pop();
                     }}
                 />
             </View>
@@ -33,15 +31,15 @@ const styles = StyleSheet.create({
         fontFamily: 'regular'
     },
     top: {
-        flex: 2,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'flex-start',
         padding: '10%'
     }, 
     bottom: {
         flex: 1,
-        backgroundColor: '#D95A2B'
+        // backgroundColor: '#D95A2B'
     }
 });
 
-export default WelcomeScreen;
+export default SelectScreen;
